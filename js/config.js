46b148b521f3b50290a6ea5da178568401b70225
Tom Schneider <ric0.github@gmail.com>
2021-10-16 22:40:54 +0200

    feat: add cognito user pool config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_uYRHUUQIa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6m6v09mh1jkkpob88aj907bore', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
